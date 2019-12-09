01b1dd467c295b9b7aaa20fb239fb7560811cb26
jakedcrampton <jakedcrampton@gmail.com>
2019-12-09 05:05:24 -0600

    Set up hyperparameter tuning for quantile size and depth

--- DecideTheWeebsExperiment.py
+++ DecideTheWeebsExperiment.py
@@ -2,30 +2,37 @@ import pandas as pd
 from decisionTree import decisionTree
 
 training = pd.read_csv("training.csv",index_col=0)
+validation=pd.read_csv("validation.csv",index_col=0)
 # attributes=training.iloc[:,4:].columns #Done in the tree
 # print(attributes)
 # print(type(attributes))
-tree=decisionTree(5)
-tree.fit(training)
+for d in [2,3,5,7,10,20]:
+    for q in [[0.5], [0.25,0.5,0.75], [0.17, .33, 0.5, .67, .83], [x/10 for x in range(10)]]:
+        tree=decisionTree(d)
+        tree.quantiles=q
+        tree.fit(training)
 
-
-testing=pd.read_csv("testing.csv",index_col=0)
-preds=[]#Correct or incorrect
-pstar=0.5
-for index, row in testing.iterrows():
-    p=tree.predict(row)
-    if(p<pstar):
-        p=0
-    else:
-        p=1
-    if(row["Male"]==p):
-        preds.append(1)
-    else:
-        preds.append(0)
-count=0
-for pred in preds:
-    count+=pred
-
-
-print("Accuracy: "+str(count/len(preds)))
-print("Most Important Column: "+tree.root.decidingColumn)
+        preds=[]#Correct or incorrect
+        pstar=0.5
+        for index, row in validation.iterrows():
+            p=tree.predict(row)
+            if(p<pstar):
+                p=0
+            else:
+                p=1
+            if(row["Male"]==p):
+                preds.append(1)
+            else:
+                preds.append(0)
+        count=0
+        for pred in preds:
+            count+=pred
+        f = open("ExperimentParameterTuning", "a")
+        s=""
+        s += "depth: "+str(d)+"\n"
+        s += "quantile set: "+str(q) + "\n"
+        s += "Accuracy: "+str(count/len(preds)) + "\n"
+        s += "Most Important Column: "+tree.root.decidingColumn + "\n"
+        print(s)
+        f.write(s+"\n\n")
+        f.close()
--- decisionTree.py
+++ decisionTree.py
@@ -8,11 +8,15 @@ class treeNode:
         self.right=None
         self.decidingColumn=""
         self.cutoff=0
+        self.p=0
 
     def choosePath(self,row: pd.core.series.Series):
         if (row[self.decidingColumn]<=self.cutoff):
             return self.left
         return self.right
+
+    def predict(self, data:pd.DataFrame):
+        self.p=data["Male"].mean() #Yeet for normalized data
 
 class leafNode:
     def __init__(self):
@@ -29,8 +33,9 @@ class decisionTree:
         self.data=None
         self.treeDepth=treeDepth #Max depth, 1 indexed, including predictor nodes
         self.sizeCutoff=30 #stop when less than this
-        self.debug=True
+        self.debug=False
         self.countNodes=0
+        self.quantiles=None
 
     def fit(self, data: pd.DataFrame):
         self.data=data
@@ -42,17 +47,30 @@ class decisionTree:
             #This note is a treeNode
             node=node.choosePath(row)
         return node.p
+    def isPerfectSplit(self, data: pd.DataFrame):
+        return len(data[data["Male"] == 1]) == len(data) or len(data[data["Male"] == 1]) == 0
 
     def makeTree(self, data: pd.DataFrame, attributes, cur_depth):
+        # Model:
+        # where Q_q represents the q quantile, which we tune during training
+        #     data[x] <= Q_q[x]?
+        #      yes/       \no
+        #   node.left   node.right
+        #
+
+        #Debug Statements
         self.countNodes+=1
         if(self.debug==True):
             print("Num Nodes: " + str(int(self.countNodes))+"/"+str(int(2**self.treeDepth-1)))
-        if cur_depth==self.treeDepth or len(data)<self.sizeCutoff:
+
+        #Finish this branch
+        if cur_depth==self.treeDepth or len(data)<self.sizeCutoff or self.isPerfectSplit(data):
             # make a prediction Node
             node=leafNode()
             node.predict(data)
             return node
 
+        #Split in two
         else:
             node=treeNode()
             #Do checks for which thing is best
@@ -62,6 +80,7 @@ class decisionTree:
             newAttributes=attributes.drop(jesusObject[1])
             node.decidingColumn=jesusObject[1]
             node.cutoff=jesusObject[2]
+            node.predict(data)
             node.left= self.makeTree(leftData,newAttributes,cur_depth+1)
             node.right= self.makeTree(rightData,newAttributes,cur_depth+1)
             return node
@@ -70,11 +89,18 @@ class decisionTree:
         best_quantile=0
         min_score=2
         answer=[] #Of form [[data1,data2], column, cutoff]
+        quantiles=[0.17, .33, 0.5, .67, .83]
+        if(not self.quantiles is None):
+            quantiles=self.quantiles
         for attribute in attributes:
-            for quantile in [0.17,.33,0.5,.67,.83]:
+            for quantile in quantiles:
                 val = data[attribute].quantile(q=quantile, interpolation='midpoint')
                 splitData=[data[data[attribute]<=val],data[data[attribute]>val]]
-                score=1*self.Remainder(splitData)
+                score=2
+                if self.isPerfectSplit(splitData[0]) or self.isPerfectSplit(splitData[1]):
+                    score=0
+                else:
+                    score=self.Remainder(splitData)
                 if(score<min_score):
                     min_score=score
                     answer=[splitData,attribute,val]
